e4647b868fb43c2860cf46265d4c6d13ff8bfb1e
hessamMahamud <h_mahamud_b@hotmail.com>
2026-08-26 18:35:36 -0500

    feat: update about view to display selected character's name and render character card

--- src/views/about.js
+++ src/views/about.js
@@ -1,11 +1,23 @@
+import { renderCharacterCard } from "../ui/characterCard.js";
+import { toCharacterProfile } from "../transform/charachter.js";
+import { CHARACTERS, DEFAULT_CHARACTER_ID } from "../data/characters.js";
+
 export function renderAbout() {
     const app = document.querySelector("#app");
+
+    const rawCharacter = CHARACTERS[DEFAULT_CHARACTER_ID];
+    const profile = toCharacterProfile(rawCharacter);
+
     app.innerHTML = `
-    <section class="view view--about">
-      <h1>Sobre este proyecto</h1>
-      <p>Esta es una POC desarrollada para ComicSansCon.</p>
-      <p>El personaje elegido es <strong>[a definir]</strong>.</p>
-      <p>Stack: HTML, CSS, JavaScript vanilla, Vercel Functions, Google Gemini.</p>
-    </section>
+        <section class="view view--about">
+            <h1>Sobre este proyecto</h1>
+            <p>Esta es una POC desarrollada para ComicSansCon.</p>
+            <p>El personaje elegido es <strong>${profile.name}</strong>.</p>
+            <p>Stack: HTML, CSS, JavaScript vanilla, Vercel Functions, Google Gemini.</p>
+            <div id="characterCardContainer"></div>
+        </section>
   `;
+
+    const cardContainer = document.querySelector("#characterCardContainer");
+    renderCharacterCard(cardContainer, profile);
 }
