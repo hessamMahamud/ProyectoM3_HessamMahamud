1fea2941ab5da7bd00c3193392b4ddf5c5b77a50
Hessam Mahamud <h_mahamud_b@hotmail.com>
2026-09-02 15:00:54 -0500

    feat: add initial test structure for getCharacterReply function

---
+++ tests/aiClient.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import {  } from "../src/features/chat/aiClient.js";
+
+describe("getCharacterReply", () => {
+    beforeEach(() => {
+        
+    })
+})
